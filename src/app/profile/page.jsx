fa44bf4667a10a99f1d649ac760482cd3ba86332
vako <96064465+vakO404@users.noreply.github.com>
2025-04-15 12:13:57 +0400

    testing vol.2

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -36,6 +36,14 @@ const Profile = async () => {
           <p>გვარი: </p>
           <p>{profile.lastName}</p>
         </div>
+        <div className={styles.infoWrapper}>
+          <p>გვარი: </p>
+          <p>{profile.lastName}</p>
+        </div>
+        <div className={styles.infoWrapper}>
+          <p>გვარი: </p>
+          <p>{profile.lastName}</p>
+        </div>
       </section>
     </div>
   );
