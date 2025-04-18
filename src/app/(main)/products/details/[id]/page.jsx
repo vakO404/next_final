4a286ec99c60b6f6af8fb4d0792e6d2f6679eef3
vakO404 <96064465+vakO404@users.noreply.github.com>
2025-04-19 01:35:57 +0400

    adding some product details, and their visuals

--- src/app/(main)/products/details/[id]/page.jsx
+++ src/app/(main)/products/details/[id]/page.jsx
@@ -1,7 +1,22 @@
-function page({params}) {
-    const id = params.id;
-    console.log(params)
-    return <div>{id}</div>;
-  }
-  
-  export default page;
+import styles from "./page.module.css";
+
+const page = async ({ params }) => {
+  const id = params.id;
+
+  const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+  const product = await res.json();
+
+  return (
+    <div className={styles.wrapper}>
+      <img className={styles.img} src={product.image} alt="product-image"/>
+      <h1 className={styles.title}>{product.title}</h1>
+      <p>category : {product.category}</p>
+      <p>{product.description}</p>
+      <strong>${product.price}</strong>
+      <strong>Rating : {product.rating.rate}⭐</strong>
+
+    </div>
+  );
+};
+
+export default page;
